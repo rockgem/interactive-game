f006a1da26cb4ac974e8afce7d5881518b3759de
rockgem <63050183+rockgem@users.noreply.github.com>
2021-11-24 19:19:56 +0800

    changes

--- scenes/levels/Control.gd
+++ scenes/levels/Control.gd
@@ -7,5 +7,5 @@ extends Control
 
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		hide()
 		$"/root/Click".play()
+		queue_free()
--- scripts/actors/LevelHolder.gd
+++ scripts/actors/LevelHolder.gd
@@ -19,6 +19,7 @@ func _on_LevelHolder_mouse_entered():
 	$Tween.interpolate_property(self, "rect_scale", null, Vector2(1.2,1.2), .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Panel/TextureRect, "rect_scale", null, Vector2(0.8,0.8), .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
+	$"/root/SwooshSfx".play()
 
 
 func _on_LevelHolder_mouse_exited():
